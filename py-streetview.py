d56f725feba60329a4bbcbf1126a70a47f2bb0cc
Tyler Kearney <75549435+darkwolf9817@users.noreply.github.com>
2024-08-14 23:00:29 -0500

    main code commit

---
+++ py-streetview.py
@@ -0,0 +1,43 @@
+import pywebview
+from flask import Flask, render_template, request, jsonify
+import geocoder
+import google_streetview
+
+app = Flask(__name__)
+
+def streetview_data(address):
+    # Geocode the address
+    geo = geocoder.google(address)
+    if geo.ok:
+        lat, lng = geo.latlng
+        
+        # Fetch streetview image
+        api_key = "AIzaSyAkdYYXm2EBigNb7xAlSoS_zGt-tsJsJDM"
+        params = [
+            {
+                "size": "600x480",
+                "location": f"{lat}, {lng}",
+                "heading": "0",
+                "pitch": "0",
+                "key": api_key
+            }
+        ]
+        results = google_streetview.api.results(params)
+        return results.download_links()
+    else:
+        return None
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        address = request.json("address")
+        image_fn = streetview_data(address)
+        return render_template("index.html", images=image_fn)
+    return render_template("index.html")
+
+def start_app():
+    pywebview.create_window("Street View Viewer", app)
+    pywebview.start
+    
+if __name__ == "__main__":
+    start_app()
